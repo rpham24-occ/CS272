3ef30998279f36f65cb5b25189bedbbb5914de5d
rpham24 <rpham24@OVDIMC10-70V.coast.cccd.adx>
2019-09-11 13:45:36 -0700

    updating

--- IC03/src/BillFold.java
+++ IC03/src/BillFold.java
@@ -4,6 +4,12 @@ public class BillFold
     private Card card1; 
     private Card card2; 
     
+    public BillFold()
+    {
+        this.card1 = null; 
+        this.card2 = null; 
+    }
+    
     public BillFold(Card card1, Card card2)
     {
         super(); 
@@ -13,11 +19,26 @@ public class BillFold
     
     public void addCard(Card c)
     {
-        
+        if(card1 == null)
+        {
+            card1 = c; 
+        }
+        else if(card2 == null)
+        {
+            card2 = c;
+        }
     }
     
     public String formatCards()
     {
-        return ""; 
+        if(card1 == null)
+        {
+            return ""; 
+        }
+        else if (card2 == null)
+        {
+            return "BillFold [" + card1.format() + "]";
+        }
+        return "BillFold [" +card1.format() + "][" + card2.format() + "]"; 
     }
 }
---
+++ IC03/src/BillFoldTest.java
@@ -0,0 +1,23 @@
+
+public class BillFoldTest
+{
+    public static void main(String[] args)
+    {
+        Card d = new DriverLicense("John Doe", "2020");
+        Card c = new CallingCard("Danny Do", "01023478", "1234");
+        Card id = new IDCard("Richard Pham", "17667954");
+        
+        BillFold bf = new BillFold();
+        bf.addCard(c);
+       
+        
+        System.out.println(bf.formatCards());
+        
+        BillFold bf2 = new BillFold();
+        bf2.addCard(c);
+        bf2.addCard(id);
+        
+        System.out.println(bf2.formatCards());
+        
+    }
+}
